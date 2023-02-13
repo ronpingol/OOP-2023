d681137ba10ef44ed3ab3e9ec48d8ebb77061e90
ronpingol <ronspingol@gmail.com>
2023-02-13 19:33:17 +0000

    week 4 class work

--- java/src/ie/tudublin/StarMap.java
+++ java/src/ie/tudublin/StarMap.java
@@ -6,20 +6,34 @@ public class StarMap extends PApplet
 {
 	public void settings()
 	{
-		size(500, 500);
+		size(800, 800);
 	}
 
 	public void setup() {
 		colorMode(HSB);
-		background(0);
+		background(255);
 
 		smooth();
 		
 	}
 
-		
+	public void drawGrid(){
+		float border = 50.0f;
+		int count = 10;
+		float gap = width - (border * 2.0f) / (float) count;
+
+		for(int i = -5; i < 5; i++){
+			
+			float x = border + (gap * (i+5));
+			line(x, border, x, height - border);
+			line(border, x, width - border, x);
+		}
+
+	}
+
 	public void draw()
 	{	
-		strokeWeight(2);		
+		strokeWeight(2);
+		drawGrid();		
 	}
 }
