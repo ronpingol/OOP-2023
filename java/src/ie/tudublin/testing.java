9a7be0882d7e87d271ced0991cc5f717286a5bbf
ronpingol <ronspingol@gmail.com>
2023-02-19 21:31:41 +0000

    processing practice

---
+++ java/src/ie/tudublin/testing.java
@@ -0,0 +1,27 @@
+package ie.tudublin;
+
+import processing.core.PApplet;
+
+public class testing extends PApplet
+{
+
+    public void settings()
+	{
+		size(640, 360);
+	}
+
+    public void setup()
+    {
+        colorMode(RGB);
+		background(255);
+    }
+
+    public void draw()
+    {
+        strokeWeight(2);
+        stroke(0);
+
+        line(10,0,10,360);
+    }
+
+}
