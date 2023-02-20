dfe820261ac5f289603e9494f05862223e64234b
ronpingol <ronspingol@gmail.com>
2023-02-20 12:46:32 +0000

    audio1

--- java/src/ie/tudublin/Audio1.java
+++ java/src/ie/tudublin/Audio1.java
@@ -1,6 +1,6 @@
 package ie.tudublin;
 
-;
+
 import ddf.minim.AudioBuffer;
 import ddf.minim.AudioInput;
 import ddf.minim.AudioPlayer;
@@ -17,14 +17,19 @@ public class Audio1 extends PApplet
 
 	public void settings()
 	{
-		size(500, 500);
+		size(1024, 500);
 	}
+
+	int frameSize = 1024;
 
 	public void setup() {
 		colorMode(HSB);
 		background(0);
 
-		
+		minim = new Minim(this);
+
+		ai = minim.getLineIn(Minim.MONO, frameSize, 44100, 16);
+		ab = ai.mix;
 
 		smooth();
 		
@@ -34,6 +39,16 @@ public class Audio1 extends PApplet
 	
 	public void draw()
 	{	
-		
+		background(0);
+		stroke(255);
+
+		float half = height / 2;
+		float cgap = 255 / (float)ab.size();
+
+		for(int i = 0; i < ab.size(); i++)
+		{
+			stroke(cgap * i, 255, 255);
+			line(i, half , i, half + ab.get(i) * half);
+		}
 	}
 }
